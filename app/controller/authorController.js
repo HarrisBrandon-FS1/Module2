72644b9e73ec8eb0aaa602725dd38da403e5422f
HarrisBrandon-FS1 <162524452+HarrisBrandon-FS1@users.noreply.github.com>
2024-10-20 21:43:18 -0500

    added config, new index, updated server to work with restfulapi

--- app/controller/authorController.js
+++ app/controller/authorController.js
@@ -1,39 +1,49 @@
-const getAllAuthors = (req, res) => {
+const Authors = require("../models/Authors");
+
+const getAllAuthors =  async (req, res) => {
+    const authors = await Authors.find({})
     res.status(200).json({
+      data: authors,
       success: true,
       message: `${req.method} - request to Author endpoint`
     });  
   };
 
-  const getAuthorById = (req, res) => {
+  const getAuthorById = async (req, res) => {
     const { id } = req.params;
+    const authorFind = await Authors.findById(id)
     res.status(200).json({
-      id,
+      authorFind,
       success: true,
       message: `${req.method} - request to Author endpoint`
     });  
   };
 
-  const createAuthor = (req, res) => {
+  const createAuthor = async (req, res) => {
+    const { author } = req.body;
+    const newAuthor = await Authors.create(author);
+    console.log("data >>>", newAuthor);
     res.status(200).json({
       success: true,
       message: `${req.method} - request to Author endpoint`
     });  
   };
 
-  const updateAuthor = (req, res) => {
+  const updateAuthor = async (req, res) => {
     const { id } = req.params;
+    const author = await Authors.findByIdAndUpdate(id,req.body, {new: true});
     res.status(200).json({
-      id,
+      data: author,
       success: true,
       message: `${req.method} - request to Author endpoint`
     });  
   };
 
-  const deleteAuthor = (req, res) => {
+  const deleteAuthor = async (req, res) => {
     const { id } = req.params;
+    const authorDelete = await Authors.findByIdAndDelete(id)    
     res.status(200).json({
-      id,
+      authorDelete,
       success: true,
       message: `${req.method} - request to Author endpoint`
     });  
--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const routeHandler = require("./routes");
+const morgan = require("morgan");
+
+
+app.use(express.json());
+app.use(morgan("dev"));
 
 
 app.get("/", (req, res) => {
